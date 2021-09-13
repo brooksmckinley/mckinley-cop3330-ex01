fe6ab7ee440b4766d88219e6fcbcd493623ff266
Brooks McKinley <brooksmckinley@yahoo.com>
2021-09-12 22:02:26 -0400

    Implemented exercise 1

--- src/main/java/com/brooksmckinley/App.java
+++ src/main/java/com/brooksmckinley/App.java
@@ -1,13 +1,19 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Brooks McKinley
+ */
+
 package com.brooksmckinley;
 
-/**
- * Hello world!
- *
- */
+import java.util.Scanner;
+
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        Scanner scan = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = scan.nextLine();
+        System.out.println("Hello, " + name + ", nice to meet you!");
     }
 }
